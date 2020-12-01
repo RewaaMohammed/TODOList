ac165076caeaf24d42f38bc3fce9d74007aed2e4
Rewaarewaa <rewaamohammed3@gmail.com>
2020-12-01 19:08:44 +0200

    start

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+Button nextbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
