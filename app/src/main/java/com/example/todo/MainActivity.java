fe0fb342714d401b4124f19a34a372d4d759cfbd
Rewaarewaa <rewaamohammed3@gmail.com>
2020-12-01 20:54:17 +0200

    start

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-Button nextbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
